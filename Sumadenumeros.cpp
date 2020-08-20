4744a9ab34d444030fa806586a1aee9b33c937bf
sthepanieas <69166217+sthepanieas@users.noreply.github.com>
2020-08-19 20:53:44 -0500

    Add files via upload

---
+++ Sumadenumeros.cpp
@@ -0,0 +1,23 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+#include <stdio.h>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a, b;
+    cout<<"Ingrese primer sumando: " ;
+    cin>> a;
+    cout<<"Ingrese segundo sumando: " ;
+    cin>> b;
+    cout<<"La suma es: " << a+b;
+    
+    return 0;
+}
