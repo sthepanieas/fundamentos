d6837cad3e24296432e1a21ad6135336855604ff
sthepanieas <69166217+sthepanieas@users.noreply.github.com>
2020-08-23 23:16:32 -0500

    Add files via upload

---
+++ Problemaseis.cpp
@@ -0,0 +1,39 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int h,m,s,x;
+    cout<<"Introduzca la hora en formato de 24 horas: "<<endl;
+    cin>>h;
+    cout<<"Introduzca los minutos: "<< endl;
+    cin>>m;
+    cout<<"Intrpduzca los segundos: "<< endl;
+    cin>>s;
+    x=h-12;
+    if (h>12) {
+        
+    cout<<"Esta es la hora: " ;
+    cout<<x;
+    cout<< ":" << m;
+    cout<<":"<< s;
+    cout<<"PM" ;
+    } else {
+    cout<<"Esta es la hora: " ;
+    cout<< h;
+    cout<<":"<< m;
+    cout<<":"<< s;
+    cout<<" AM"; }}
+
+
+
+
